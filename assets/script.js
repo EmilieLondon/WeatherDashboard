0d738f2235d55803f57386c505af2670a94ed46a
Emilie Godin-Depaul <em.godin80@yahoo.com>
2023-01-30 12:53:16 +0000

    setting up URLs with API key, creating buttons for city list, obtaining weather icon

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,74 @@
 // API Key
-let apiKey = "";
+let apiKey = "82c5cfb47d6e85cd369108ab5201240b";
+
 // DOM elements
-let searchButton = document.querySelector("#search-button");
-let searchInput = document.querySelector("#search-input");
-let cityList = document.querySelector(".city-list");
-// addEventListener on search button
-searchButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  // store searchInput into variable
-  let city = searchInput.value;
-  console.log(city);
-  // let city = “London”;
-  // URL 1 build
+
+let cityButtons = document.querySelector(".city-buttons");
+let weatherToday = document.querySelector("#today");
+
+let cities = [];
+
+
+// check the local storage and render city list
+init();
+function init() {
+  let savedCities = JSON.parse(localStorage.getItem("cities"));
+  if (savedCities) {
+    cities = savedCities;
+  }
+  renderCities(cities);
+}
+
+//create buttons
+function renderCities(cities) {
+  cityButtons.innerHTML = "";
+  for (let i = 0; i < cities.length; i++) {
+    const city = cities[i];
+    let cityButton = document.createElement("li");
+    cityButton.innerHTML = city;
+    cityButtons.prepend(cityButton);
+  }
+}
+
+  // linking 2 URLs to obtain required data
   let queryURL1 =
-    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=` +
+    `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=` +
     apiKey;
-  // We need this data first in order to make the 2nd request
-  fetch(queryURL1)
+  
+    fetch(queryURL1)
     .then((response) => response.json())
     .then((citiesFound) => {
       let firstCity = citiesFound[0];
+      console.log(firstCity);
       console.log(firstCity.lat);
       console.log(firstCity.lon);
-      // 2nd URL data request chained onto 1st URL data request
+
       let queryURL2 =
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=` +
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&units=metric&appid=` +
         apiKey;
+
       return fetch(queryURL2);
     })
+
     .then((response) => response.json())
     .then((cityData) => {
-      // the below is the data from return fetch (queryURL2)
       console.log(cityData);
+      renderWeather(cityData);
     });
-  // Create elements for every city searched
-  let cityButton = document.createElement("li");
-  // Assign text value
-  cityButton.innerHTML = city;
-  // Prepend every city
-  cityList.prepend(cityButton);
-  // Empty array to store cities in localStorage
-  let cities = [];
-  // Push cities searched into this array
-  cities.push(city);
-  // Function to save cities to localStorage
-  function storeCitites() {
-    localStorage.setItem("cities", JSON.stringify(cities));
-    console.log(localStorage);
-  }
-  storeCitites();
-  // Function to retrieve cities from localStorage
-});
+
+//obtaining weather icon
+function renderWeather(weatherData) {
+  let cityName = weatherData.city.name;
+  let iconCode = weatherData.list[0].weather[0].icon;
+  let iconURL = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+  console.log(cityName);
+  let html = `<h1>${cityName} (${moment(weatherData.dt).format(
+    "DD/MM/YYYY"
+  )}) <img src='${iconURL}'></h1>
+  <p>Temp: ${Math.floor(weatherData.list[0].main.temp)} &#8451</p>
+  <p>Wind</p>
+  <p>Humidity</p>`;
+
+  weatherToday.innerHTML = html;
+}
